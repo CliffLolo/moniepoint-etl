81f44501cb12c8d2e45ab0c4ecd0cda0a0ed21ed
Clifford Frempong <cliffordfrempong@Cliffords-MacBook-Pro-2.local>
2023-12-04 14:18:35 +0200

    formatting the scripts using black

--- dags/main.py
+++ dags/main.py
@@ -4,29 +4,34 @@ from airflow.operators.python import PythonOperator
 from scripts.etl import extract_data, insert_data
 
 default_args = {
-    'owner': 'Clifford Frempong',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "owner": "Clifford Frempong",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 1, 1),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
-with DAG('moniepoint-etl-dag', start_date=datetime(2022, 1, 1), default_args=default_args, description='This Airflow DAG orchestrates the execution of an ETL process. The ETL process extracts data from a ClickHouse database, performs necessary transformations, and loads the results into a SQLite database.',
-        schedule_interval=None, catchup=False) as dag:
-
+with DAG(
+    "moniepoint-etl-dag",
+    start_date=datetime(2023, 1, 1),
+    default_args=default_args,
+    description="This Airflow DAG orchestrates the execution of an ETL process. The ETL process extracts data from a ClickHouse database, performs necessary transformations, and loads the results into a SQLite database.",
+    schedule_interval=None,
+    catchup=False,
+) as dag:
     # Task 1: Extract data
     extract = PythonOperator(
-        task_id='extract',
+        task_id="extract",
         python_callable=extract_data,
     )
 
     # Task 2: Load data
     load = PythonOperator(
-        task_id='load',
-        python_callable=insert_data, 
-        op_args=[extract.output],  
+        task_id="load",
+        python_callable=insert_data,
+        op_args=[extract.output],
         provide_context=True,
     )
 
--- scripts/etl.py
+++ scripts/etl.py
@@ -6,17 +6,22 @@ from dotenv import load_dotenv
 # Loading the environment variables from the .env file
 load_dotenv()
 
-CLICKHOUSE_CLOUD_HOSTNAME = os.getenv('CLICKHOUSE_CLOUD_HOSTNAME')
-CLICKHOUSE_PORT = int(os.getenv('CLICKHOUSE_PORT'))
-CLICKHOUSE_USERNAME = os.getenv('CLICKHOUSE_USERNAME')
-CLICKHOUSE_PASSWORD = os.getenv('CLICKHOUSE_PASSWORD')
-DATABASE_NAME = os.getenv('DATABASE_NAME')
+CLICKHOUSE_CLOUD_HOSTNAME = os.getenv("CLICKHOUSE_CLOUD_HOSTNAME")
+CLICKHOUSE_PORT = int(os.getenv("CLICKHOUSE_PORT"))
+CLICKHOUSE_USERNAME = os.getenv("CLICKHOUSE_USERNAME")
+CLICKHOUSE_PASSWORD = os.getenv("CLICKHOUSE_PASSWORD")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
 
 def extract_data():
     client = clickhouse_connect.get_client(
-        host=CLICKHOUSE_CLOUD_HOSTNAME, port=CLICKHOUSE_PORT, username=CLICKHOUSE_USERNAME, password=CLICKHOUSE_PASSWORD)
+        host=CLICKHOUSE_CLOUD_HOSTNAME,
+        port=CLICKHOUSE_PORT,
+        username=CLICKHOUSE_USERNAME,
+        password=CLICKHOUSE_PASSWORD,
+    )
 
-    QUERY = '''
+    QUERY = """
         SELECT
             DATE_FORMAT(pickup_date, '%Y-%m') AS month,
             AVG(CASE WHEN DAYOFWEEK(pickup_date) = 7 THEN 1 ELSE 0 END) AS sat_mean_trip_count,
@@ -33,16 +38,18 @@ def extract_data():
             DATE_FORMAT(pickup_date, '%Y-%m')
         ORDER BY
             month
-    '''
+    """
 
     result = client.query(QUERY)
     return result.result_rows
+
 
 def insert_data(data):
     sqlite_conn = sqlite3.connect(DATABASE_NAME)
     sqlite_cursor = sqlite_conn.cursor()
 
-    sqlite_cursor.execute('''
+    sqlite_cursor.execute(
+        """
     CREATE TABLE IF NOT EXISTS moniepoint_metrics (
         month TEXT,
         sat_mean_trip_count REAL,
@@ -52,12 +59,16 @@ def insert_data(data):
         sun_mean_fare_trip REAL,
         sun_mean_duration_per_trip REAL
     )
-    ''')
+    """
+    )
 
     for row in data:
-        sqlite_cursor.execute('''
+        sqlite_cursor.execute(
+            """
         INSERT INTO moniepoint_metrics VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', row)
+        """,
+            row,
+        )
 
     sqlite_conn.commit()
     sqlite_conn.close()
